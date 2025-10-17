56e9048fa08e599e756b5d4f425622df4c11fcc3
Travis <mynameismyname1992@gmail.com>
2025-10-16 19:48:03 -0500

    Created menu and basic code structure

---
+++ expense tracker/main.py
@@ -0,0 +1,17 @@
+def menu():
+    print("\n----- Personal Expense Tracker -----")
+    print("\n1. Add expense")
+    print("2. View all expenses")
+    print("3. Calculate total spent")
+    print("4. Exit")
+    choice = input("")
+    return choice
+
+def add_expense():
+    pass
+
+def view_expenses():
+    pass
+
+def calc_total():
+    pass
