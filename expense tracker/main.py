d8113af5a413d3e7d5f5c34d4177faa52254c2fe
Travis <mynameismyname1992@gmail.com>
2025-10-16 20:23:45 -0500

    Created ExpenseTracker class skeleton and menu() method

--- expense tracker/main.py
+++ expense tracker/main.py
@@ -1,17 +1,52 @@
-def menu():
-    print("\n----- Personal Expense Tracker -----")
-    print("\n1. Add expense")
-    print("2. View all expenses")
-    print("3. Calculate total spent")
-    print("4. Exit")
-    choice = input("")
-    return choice
+class ExpenseTracker:
+    def __init__(self):
+        self.expenses = []
 
-def add_expense():
-    pass
+    def add_expense(self):
+        # TODO: ask for amount, category, description and append to self.expenses
+        pass
 
-def view_expenses():
-    pass
+    def view_expenses(self):
+        # TODO: print all expenses
+        pass
 
-def calc_total():
-    pass
+    def filter_by_category(self):
+        # TODO: filter expenses by category
+        pass
+
+    def show_total(self):
+        # TODO: sum all amounts and print total
+        pass
+
+    def menu(self):
+        print("\n----- Expense Tracker -----")
+        print("1. Add Expense")
+        print("2. View All Expenses")
+        print("3. Filter by Category")
+        print("4. Show Total Spent")
+        print("5. Exit")
+        choice = input("Enter choice: ")
+        return choice
+    
+def main():
+    tracker = ExpenseTracker() # Creates object of the class
+
+    while True:
+        choice = tracker.menu()
+
+        if choice == "1":
+            tracker.add_expense()
+        elif choice == "2":
+            tracker.view_expenses()
+        elif choice == "3":
+            tracker.filter_by_category()
+        elif choice == "4":
+            tracker.show_total()
+        elif choice == "5":
+            print("Exiting Program.")
+            break
+        else:
+            print("Invalid choice. Try again.")
+
+if __name__ == "__main__":
+    main()
