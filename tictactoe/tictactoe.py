2df84e995b759ffa20bfadb7dfcb4190755136fa
Travis <mynameismyname1992@gmail.com>
2025-10-30 08:53:21 -0500

    Add nested loop to iterate through each element on the board, Print player symbols 'X', 'O' onto board

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -6,46 +6,20 @@ def new_board():
 ]
 
 def render(board):
-    print("""
-          0 1 2
-         -------
-        0|     |
-        1|     |
-        2|     |
-         -------
-""")
+    print("    0   1   2")                                          # Print column numbers
+    print("  -------------")                                        # Print top border
+    for i, row in enumerate(board):                                 # Go through each row
+        # Replace None or "" with a space
+        symbol = [cell if cell else " " for cell in row]       # Use nested loop to iterate through each cell in the row
+        # Use f string to keep spacing even
+        print(f"{i} | {' | '.join(symbol)} |")
+
+        print("  -------------")                     # Print bottom border
 
 board = new_board()
-board[0][1] = 'X'
+board[0][2] = 'X'
 board[1][1] = 'O'
 render(board)
-
-# Loop through turns until the game is over
-#while True:
-
-    # Print the current state of the board
-    #render(board)
-
-    # Get the move that the current player is going
-    # to make.
-    #move_co_ords = get_move()
-
-    # Make the move that we calculated above
-    #make_move(board, move_co_ords)
-
-    # Work out if there's a winner
-    #winner = check_win(board)
-
-  # If there is a winner, crown them the champion
-  # and exit the loop.
-
-
-  # If there is no winner and the board is full,
-  # exit the loop.
-
-  # Repeat until the game is over
-
-
 
 
 
